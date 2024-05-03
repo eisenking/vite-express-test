3e6c9d3bb22504d40a3a09ddd666b765a324978b
Vitaly <eisenking@yandex.ru>
2024-05-03 21:59:49 +0500

    render.com 3rd deploy test - post order / register fix

--- src/server/utils/generateToken.js
+++ src/server/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (res, userId) => {
-  const token = jwt.sign({ userId }, `${process.env.JWT_SECRET}`, {
+  const token = jwt.sign({ userId }, `jwt-secret-key`, {
     expiresIn: '30d',
   });
   console.log(token)
